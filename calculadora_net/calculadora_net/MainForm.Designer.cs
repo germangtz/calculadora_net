02822d376b3d76dd5d5d09c07c0c5ae8cfa74088
germangtz <licgerman@gmail.com>
2025-11-27 16:47:02 -0600

    Se agregó la funcionalidad de suma de dos numeros

--- calculadora_net/calculadora_net/MainForm.Designer.cs
+++ calculadora_net/calculadora_net/MainForm.Designer.cs
@@ -36,12 +36,61 @@ namespace calculadora_net
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(171, 160);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(182, 71);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "SUMAR";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(171, 79);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(178, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(172, 106);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(176, 22);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(213, 258);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "label1";
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "calculadora_net";
+			this.ClientSize = new System.Drawing.Size(493, 371);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
+			this.Text = "calculadora_net";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
